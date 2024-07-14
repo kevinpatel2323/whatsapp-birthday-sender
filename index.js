76da96ff47e9acd698f8f1711ae757b3ade98072
kevinpatel2323 <kevinpatelcse@gmail.com>
2024-07-14 20:03:00 +0530

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
-const dotenv = require('dotenv');
-dotenv.config();
 const port = process.env.PORT || 3000;
 
 // Serve static files from the "public" directory
@@ -10,11 +8,15 @@ app.use(express.static('public'));
 
 // MySQL connection setup
 const connection = mysql.createConnection({
-    host: 'mysql-1b961294-kevinpatelsmsm.i.aivencloud.com',
-    user: 'avnadmin',
-    password: 'AVNS_TXCNk41R7UU80GqmXJv',
-    database: 'defaultdb'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: 3306,
+    connectTimeout: 10000
 });
+
+console.log('Attempting to connect to MySQL...');
 
 connection.connect((err) => {
     if (err) {
@@ -24,7 +26,7 @@ connection.connect((err) => {
     console.log('Connected to MySQL');
 });
 
-app.get('/', (req, res) => {
+app.get('/users', (req, res) => {
     connection.query('SELECT * FROM users', (error, results) => {
         if (error) {
             return res.status(500).json({ error });
