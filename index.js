4bdf105cccf4e0953ee839920980991929e4b454
kevinpatel2323 <kevinpatelcse@gmail.com>
2024-07-14 21:47:37 +0530

    first commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: 3306,
+    port: 19677,
     connectTimeout: 10000
 });
 
